19faaa19656e6ca8734995a78ee0cb2d793d994b
nahar-2001 <nahazette@gmail.com>
2024-03-15 16:49:19 +0800

    category added

--- Admin/8-report.php
+++ Admin/8-report.php
@@ -30,28 +30,39 @@ if (isset($_POST['submit'])) {
 <body>
     <h1>REPORT</h1>
 
-    <form action="" method="post" enctype="multipart/form-data">
-    <div>
-        <p><label for="title">Title</label></p>
-        <textarea name="title" id="title" cols="30" rows="10"></textarea>          
-    </div>
-    <div>
-        <p><label for="reports">Write Your Reports</label></p>
-        <textarea name="reports" id="reports" cols="30" rows="10"></textarea>
-    </div>
-    <div>
-        <p><label for="reports">Conclusion</label></p>
-        <textarea name="reports" id="reports" cols="30" rows="10"></textarea>
-    </div>
-    <div>
-        <p><label for="gambar">Gambar</label></p>
-        <input type="file" name="gambar" id="gambar">
-    </div>
+<form action="" method="post" class="row" enctype="multipart/form-data">
 
-    <br>    
-    <br>
-    <button type="submit" name="submit">Submit</button>
+<div class="form-group col-md-6">
+                                    <label for="exampleInputEmail1">Post Title</label>
+                                    <input type="text" class="form-control" id="posttitle" name="posttitle" placeholder="Enter title" required>
+                                 </div>
+<div class="form-group col-md-6">
+    <label for="exampleInputEmail1">Category</label>
+    <select class="form-control" name="category" id="category" onChange="getSubCat(this.value);" required>
+    <option value="">Select Category </option>
+    </select>
+</div>
+<div class="form-group col-md-6">
+    <label for="exampleInputEmail1">Sub Category</label>
+    <select class="form-control" name="subcategory" id="subcategory" required></select> 
+</div>
+<div class="row">
+     <div class="col-sm-12">
+        <div class="card-box">
+            <h4 class="m-b-30 m-t-0 header-title"><b>Post Details</b></h4>
+            <textarea class="summernote" name="postdescription" required></textarea>
+        </div>
+    </div>
+</div>
+<div>
+    <h4><b>Feature Images</b></h4>
+    <input type="file" class="form-control" name="postimage" id="postimage" required>
+</div>
 
-    </form>
+<button type="submit" name="submit" class="btn btn-customn waves-effect waves-light btn-md">Save and Post</button>
+<br>
+<button type="button" class="btn btn-danger waves-effect waves-light">Discard</button>
+</form>    
+
 </body>
 </html>
